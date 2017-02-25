e1d3b9832773eb41f8a019202103c3b636bf1f6c
anuj4994 <anuj.shah.94@gmail.com>
2017-02-24 20:55:49 -0500

    Adding more Challenges

---
+++ src/hackerrank/CircularArrayRotationAlgos.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Anuj Shah
+ */
+public class CircularArrayRotationAlgos {
+     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int q = in.nextInt();
+        ArrayList<Integer> a = new ArrayList<>();
+        int[] m = new int[q];
+        for(int a_i=0; a_i < n; a_i++){
+            a.add(in.nextInt());
+        }
+        
+        Collections.rotate(a, k);
+         
+        for(int a0 = 0; a0 < q; a0++){
+            m[a0] = in.nextInt();
+        }
+        for(int a0 = 0; a0 < q; a0++){
+            System.out.println(a.get(m[a0]));
+        }
+    }
+}
---
+++ src/hackerrank/TimeConversionAlgos.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Anuj Shah
+ */
+public class TimeConversionAlgos {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String time = sc.nextLine();
+        String[] splittedTime = time.split(":");
+        int hours = Integer.parseInt(splittedTime[0]);
+        int minutes = Integer.parseInt(splittedTime[1]);
+        int seconds = Integer.parseInt(splittedTime[2].substring(0,2)); // It will take 2 digits of seconds from last part. 
+        String ampm = splittedTime[2].substring(2); //It will take AM or PM from the String.
+        if(ampm.equals("AM")){
+            if(hours == 12){
+                hours = 00;
+            }
+        } else {
+            if(hours != 12){
+               hours += 12;
+            } 
+            
+        }
+        System.out.println(String.format("%02d", hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds));
+    }
+}
